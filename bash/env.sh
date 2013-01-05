64e765df065a37d2086daaa36be8af9dbf934530
André Luiz Pardal <alpardal@gmail.com>
2013-01-04 22:50:58 -0200

    add scala and postgres to path

--- bash/env.sh
+++ bash/env.sh
@@ -17,7 +17,8 @@ export HISTSIZE=10000
 
 export JAVA_HOME=~/.jdk1.7.0
 export JAVA_OPTS="-client"
+export POSTGRES_PATH="/usr/local/pgsql/bin"
+export PGDATA="/usr/local/pgsql/data"
 export USER_BIN=~/.bin
-export PATH=$JAVA_HOME/bin:$PATH:$USER_BIN
-
-export PS1="\[\e[0;34m\][\u]\[\e[1;34m\]\w\[\e[0m\] ☢ "
+export SCALA_HOME=~/Downloads/scala-2.9.2
+export PATH=$POSTGRES_PATH:$JAVA_HOME/bin:$PATH:$SCALA_HOME/bin:$USER_BIN
