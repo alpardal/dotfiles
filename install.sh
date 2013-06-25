859fc0004d75089b6f5a3acd98052b340b4f4aba
André Luiz Pardal <alpardal@gmail.com>
2013-06-25 20:56:47 -0300

    add .railsrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in .irbrc .gemrc .tmux.conf .bashrc .vimrc .zshrc; do
+for file in .irbrc .gemrc .tmux.conf .bashrc .vimrc .zshrc .railsrc; do
     if [ -f $file ]; then
         mv $HOME/$file $HOME/${file}_old
     fi
@@ -11,6 +11,7 @@ PWD="`pwd`"
 ln -fs $PWD/irbrc          $HOME/.irbrc
 ln -fs $PWD/zshrc          $HOME/.zshrc
 ln -fs $PWD/gemrc          $HOME/.gemrc
+ln -fs $PWD/railsrc        $HOME/.railsrc
 ln -fs $PWD/tmux.con       $HOME/.tmux.conf
 ln -fs $PWD/bash/bashrc.sh $HOME/.bashrc
 ln -fs $PWD/vim/vimrc.vim  $HOME/.vimrc
