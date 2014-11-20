62a67daea9149ce93d2d21406c8d62950369bca6
André Luiz Pardal <alpardal@gmail.com>
2014-11-20 16:27:05 -0200

    formatting

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -3,15 +3,16 @@ if [ -x /usr/bin/dircolors ]; then
     eval "`dircolors -b`"
     alias ls='ls -xX --color=auto'
 fi
-alias quit=" exit"
-alias sa="sudo apt-get"
+alias quit=' exit'
+alias tree=' tree --dirsfirst'
+alias sa='sudo apt-get'
 alias ll='ls -oh'
 alias la='ls -A'
-alias l1="ls -F -1 --group-directories-first"
-alias ack="ack-grep"
-alias a="ack-grep"
+alias l1='ls -F -1 --group-directories-first'
+alias ack='ack-grep'
+alias a='ack-grep'
 alias -- -='cd -'
-alias re.=". ~/.bashrc"
+alias re.='. ~/.bashrc'
 alias fg=' fg'
 
 alias vi=' vi'
@@ -20,9 +21,9 @@ alias vi=' vi'
 alias golang='/usr/local/go/bin/go'
 
 # ruby
-alias ri="ri -Tf ansi"
-alias gemset="rvm gemset"
-alias serve="ruby -run -e httpd . -p 3000"
+alias ri='ri -Tf ansi'
+alias gemset='rvm gemset'
+alias serve='ruby -run -e httpd . -p 3000'
 # rails
 alias r='bin/rails'
 alias rs='bin/rails s'
@@ -32,29 +33,29 @@ alias rdc='bin/rails dbconsole'
 alias rk='bin/rake'
 
 # git
-alias gs="git status"
-alias gss="git status --short"
-alias ga="git add"
-alias gaa="git add --all"
-alias gb="git branch"
+alias gs='git status'
+alias gss='git status --short'
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
 alias gl="git --no-pager log --oneline --decorate --graph --branches='*' -30"
 alias glf="git log --oneline --decorate --graph --branches='*'"
-alias go="git checkout"
-alias gi="git commit"
-alias gd="git diff"
-alias gdc="git diff --word-diff-regex=." # char diff
-alias gdp="git diff --patience"
-alias gds="git diff --staged"
-alias gdw="git diff --ignore-all-space"
-alias gm="git merge"
-alias gt="git tag"
-alias stash="git stash -u"
-alias stashp="git stash pop"
-alias stashl="git stash list --oneline --decorate"
+alias go='git checkout'
+alias gi='git commit'
+alias gd='git diff'
+alias gdc='git diff --word-diff-regex=.' # char diff
+alias gdp='git diff --patience'
+alias gds='git diff --staged'
+alias gdw='git diff --ignore-all-space'
+alias gm='git merge'
+alias gt='git tag'
+alias stash='git stash -u'
+alias stashp='git stash pop'
+alias stashl='git stash list --oneline --decorate'
 #tmux
-alias tm="tmux"
-alias ts="tmux list-sessions"
-alias ta="tmux attach"
+alias tm='tmux'
+alias ts='tmux list-sessions'
+alias ta='tmux attach'
 # other
-alias ctags="ctags-exuberant --extra=+f -R"
-alias node="node --harmony"
+alias ctags='ctags-exuberant --extra=+f -R'
+alias node='node --harmony'
