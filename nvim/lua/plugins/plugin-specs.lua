19521892e2d6b0ffa98ed7efaf7a1efd1f3c7b1a
André Luiz Pardal <alpardal@gmail.com>
2025-07-27 16:55:29 -0300

    wip: ai stuff

--- nvim/lua/plugins/plugin-specs.lua
+++ nvim/lua/plugins/plugin-specs.lua
@@ -306,4 +306,39 @@ return {
     --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     -- end,
   },
+  { "github/copilot.vim" },
+  {
+    "olimorris/codecompanion.nvim",
+    config = function()
+      require("codecompanion").setup({
+        extensions = {
+          mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+              make_vars = true,
+              make_slash_commands = true,
+              show_result_in_chat = true,
+            },
+          },
+        },
+      })
+    end,
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    dependencies = {
+      "ravitemer/mcphub.nvim",
+    },
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function()
+      require("mcphub").setup()
+    end,
+  },
 }
